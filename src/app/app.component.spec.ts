2921599b92335d8af318ceb5b6e6600969630433
Johnny Cowboy <johnnycowboy3033@gmail.com>
2022-12-30 23:40:46 -0500

    init commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,33 @@
-import { TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 describe('AppComponent', () => {
+  let component : AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+      ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+  });
+
+  beforeEach( ()=> {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+  });
+
+  it('should be true', () => {
+    const testVariable = true;
+    expect(testVariable).toEqual(testVariable);
   });
 
   it('should create the app', () => {
@@ -28,4 +48,6 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('star app is running!');
   });
+
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+
+interface Team{
+  id:number,
+  name:string
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'star';
+
+  constructor( private fb: FormBuilder) {
+  }
+
+  teamList :Team[] = [
+    {
+      id:1,
+      name: 'Red Team',
+    },
+    {
+      id:2,
+      name: 'Blue Team',
+    }
+
+  ];
+
+  registerForm = this.fb.group({
+    username: [null, [Validators.required, Validators.maxLength(10)]],
+    password: [null, [Validators.required] ],
+    email: [null, [Validators.required] ],
+    gender:['M'],
+    team: [null, [Validators.required] ],
+    subscription:[true]
+  });
+
+  submitForm(){
+
+  }
+
 }
